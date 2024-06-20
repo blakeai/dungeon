b857e737495ea2647fe2bb75483919335b28977d
Blake Icabone <b.icabone@gmail.com>
2024-06-19 20:56:55 -0400

    test .env

--- src/utils/env_utils.rs
+++ src/utils/env_utils.rs
@@ -1,16 +1,16 @@
 use std::env;
 use std::path::Path;
 
-use dotenv::dotenv;
+use dotenv::from_filename;
 
 pub(crate) fn load_env(variable_name: String) -> String {
-    dotenv().ok();
+    let env_file = if cfg!(test) { ".env.test" } else { ".env" };
+    from_filename(env_file).ok();
     let value = env::var(variable_name).unwrap();
     return value;
 }
 
 pub(crate) fn load_filename(variable_name: String) -> String {
     let filename = load_env(variable_name);
-    let path_buf = Path::new("resources").join(filename);
-    return path_buf.to_str().unwrap().to_string();
+    return Path::new(&filename).to_str().unwrap().to_string();
 }
